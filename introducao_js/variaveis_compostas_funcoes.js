523fe094a160ae4f3d9d7cb0699ad37b85408c5f
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-08-23 09:47:33 -0300

    adicionar

---
+++ introducao_js/variaveis_compostas_funcoes.js
@@ -0,0 +1,15 @@
+// declara um array
+const notas =[9,8.5,7,"10"];
+
+notaas[4] = true;
+
+/**
+ * comentário bloco
+ */
+
+console.log(notas);
+
+// verifica a quantidade de elementos array
+console.log(notas.length);
+
+
