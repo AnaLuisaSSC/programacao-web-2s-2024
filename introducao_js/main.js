201cdcb1be24ed61e31f913461e9d5a0e7955a11
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-08-30 10:48:15 -0300

    adicionar

---
+++ introducao_js/main.js
@@ -0,0 +1,8 @@
+const calc = require('./calculadora');
+
+let a = 2;
+let b = 3;
+
+console.log(`a soma entre $(a) e $(b) é $(calc.somar(a, b))`);
+console.log('a multiplicação $(a) e $(b) é $[calc.somar(a, b)]');
+console.log('PI = " + calc');
