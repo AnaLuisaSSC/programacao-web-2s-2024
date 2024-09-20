b37d0aa0f22ea62d1adacf86d4b3dfe876f04066
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-09-20 11:08:39 -0300

    commit

--- app_html/index.js
+++ app_html/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const app = express();
 
-app.get('/', (req, res))
-    res.send('App HTML')
-    
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/viwes');
+
+app.get('/', (req, res)=>{
+     res.render('index.html');
+});
+   
 const PORT = 8086;
 app.listen(PORT, ()=>{
-    console.log('app executando na porta' + PORT)
+    console.log('app executando na porta' + PORT);
 })
