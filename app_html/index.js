7a6034a448eeeaf4b7edbebe7d50045ba4abd0f2
AnaLuisaSSC <analuisasantoscorrea@gmail.com>
2024-10-11 10:57:56 -0300

    commint

--- app_html/index.js
+++ app_html/index.js
@@ -7,7 +7,8 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/viwes');
 
 app.get('/', (req, res)=>{
-     res.render('index.html');
+    let erro_form = req.query.erro_form;
+     res.render('index.html', {erro_form});
 });
    
 app.post('\dados', (req, res)=>{
@@ -16,10 +17,27 @@ app.post('\dados', (req, res)=>{
         data_nascimento: red.boddy.data_nascimento,
         descricao: req.body.descricao
     };
-    res.send(dados);
+    
+    let erro_form = false;
+    if(dados.nome.length == 0){
+        erro_form = true;
+    }
+    if(dados.data_nascimento.length == 0){
+        erro_form = true;
+    }
+    if(dados.descricao.length == 0){
+        erro_form = true;
+    }
+    if(erro_form){
+        res.redirect('/?erro_form=true');
+    }
+    else{
+        res.send('dados.html', {dados});
+    }
+    
 });
 
-const PORT = 8088;
+const PORT = 8080;
 app.listen(PORT, ()=>{
-    console.log('app executando na porta' + PORT);
+    console.log('app rodando na porta' + PORT);
 });
